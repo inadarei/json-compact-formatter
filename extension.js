7c7efc6ff4173da76f622e49ffa4dcded5f4ef1c
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2022-08-07 15:52:13 -0400

    fixing #3

--- extension.js
+++ extension.js
@@ -10,14 +10,12 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    //console.log('Congratulations, your extension "json-compact-prettifier" is now active!');
+    console.log('Congratulations! Your extension "json-compact-prettifier" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('json-compact-prettifier.prettyFormatCompact', function () {
-
-        vscode.window.showInformationMessage("command format json registering");
 
         // The code you place here will be executed every time your command is executed
         var srcText = getSelectionText();
@@ -69,9 +67,10 @@ function activate(context) {
      return srcText;
  }
 
-exports.activate = activate;
-
 // this method is called when your extension is deactivated
 function deactivate() {
 }
-exports.deactivate = deactivate;
+module.exports = {
+	activate,
+	deactivate
+}
